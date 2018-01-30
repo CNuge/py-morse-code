ec32651603be0287c2f6656669a970f1c486c326
WATTBA <nugentc@uoguelph.ca>
2018-01-30 16:03:19 -0500

    comments on tests

--- tests.py
+++ tests.py
@@ -1,10 +1,9 @@
 from morse import Morse, DotDash
 import unittest
 
-
-
 class MorseCodeTests(unittest.TestCase):
-	""" """
+	""" unit tests for the reading, translation and representation of the
+		Morse code class"""
 	@classmethod
 	def setUpClass(self):
 		""" this sets up a class through each of the initiation methods, making
@@ -18,12 +17,18 @@ class MorseCodeTests(unittest.TestCase):
 		self.morse_test5 = Morse()
 		self.morse_test5.read(morse = '... --- ...')
 
-	
 	def test_encoding(self):
+		""" make sure the translation from morse to words, 
+			and words to morse have occured """
 		self.assertEqual(self.morse_test1.words, 'sos 1')
 		self.assertEqual(self.morse_test1.morse, '... --- ... / .----')
 	
+	def test_double_pass(self):
+		with self.assertRaises(ValueError):
+			self.morse_test6.read(words= 'we are passing two things ', morse = '... --- ...')
+
 	def test_properties(self):
+		""" make sure we can't overwrite the stored strings """
 		with self.assertRaises(AttributeError):
 			self.morse_test1.words = 'change the words'
 
@@ -31,11 +36,14 @@ class MorseCodeTests(unittest.TestCase):
 			self.morse_test1.morse = '... --- ...'
 
 	def test_repr(self):
+		""" make sure that __repr__ is printing the expected message 
+			when print(object) is called. """
 		self.assertEqual(self.morse_test2.__repr__(), 'message: you never did the kenosha kid\n-.-- --- ..- / -. . ...- . .-. / -.. .. -.. / - .... . / -.- . -. --- ... .... .- / -.- .. -..' )
 
 
 test_test = MorseCodeTests()
 test_test.setUpClass()
+test_test.test_double_pass()
 test_test.test_encoding()
 test_test.test_properties()
 test_test.test_repr()
